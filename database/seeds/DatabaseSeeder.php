72d4ef05775aee33bee52b842ad5a51c4ac936d3
Daniel <dani.q.h@gmail.com>
2016-08-13 22:29:25 +0100

    Added Method to truncating table with seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->truncateTables(array(
+            'users',
+            'password_resets',
+            'tickets',
+            'ticket_votes',
+            'ticket_comments'
+
+        ));
+
+        $this->call(UserTableSeeder::class);
+    }
+
+    private function truncateTables(array $tables)
+    {
+        $this->checkForeignKeys(false);
+
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+
+        DB::table('users')->truncate();
+
+        $this->checkForeignKeys(true);
+    }
+
+    private function checkForeignKeys($check)
+    {
+        $check=$check?'1' : '0';
+        DB::statement('SET FOREIGN_KEY_CHECKS='.$check);
     }
 }
